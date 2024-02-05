8b38414af10b5284c0770d1e678b531b5e444169
jessi <jessi.nowski@hotmail.de>
2024-02-05 14:33:00 +0100

    new class BankService

--- src/main/java/com/github/BankService.java
+++ src/main/java/com/github/BankService.java
@@ -1,12 +1,61 @@
 package com.github;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class BankService {
-    //Create a class 'BankService' that manages a set of accounts.
+    // Step 5:
+    // Create a class 'BankService' that manages a set of accounts.
     private Set<Account> accounts = new HashSet<>();
 
-    //Implement a method in BankService to open an account.
-    //It should only require a customer as an argument and should return the new account number.
+    // Step 6:
+    // Implement a method in BankService to open an account.
+    // It should only require a customer as an argument and should return the new account number.
+
+    public String newAccountNumber(Client client){
+        String newAccountNumber = "22"+client.customerNumber();
+        return newAccountNumber;
+    }
+    // Step 7:
+    // Implement a method in BankService to transfer a specific amount (as BigDecimal)
+    // from one account number (as String) to another account number (as String).
+
+    // ...in Bearbeitung...
+
+    public BigDecimal transfer(BigDecimal amount, String fromAccountNumber, String toAccountNumber){
+        return null;
+    }
+
+    //Getter und Setter
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankService that = (BankService) o;
+        return Objects.equals(accounts, that.accounts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accounts);
+    }
+
+    @Override
+    public String toString() {
+        return "BankService{" +
+                "accounts=" + accounts +
+                '}';
+    }
+
+
+
+
 }
--- src/main/java/com/github/Main.java
+++ src/main/java/com/github/Main.java
@@ -1,5 +1,7 @@
 package com.github;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         // Step 1:
@@ -8,6 +10,15 @@ public class Main {
 
         Client jessica = new Client("Jessica","Zarnowki", 1804);
         Client hanne = new Client("Hanne","Lore", 1922);
+
+        // Step 6:
+        // Implement a method in BankService to open an account.
+        BankService haspa = new BankService();
+
+
+        Account myAccount = new Account(haspa.newAccountNumber(jessica), new BigDecimal(0), jessica);
+
+        System.out.println(myAccount.deposit(new BigDecimal(200)));
 
     }
 }
